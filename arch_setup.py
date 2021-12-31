4f2a1cbc2d49a02d1b4349f1d22337db0a47dfbf
me <a@a.a>
2021-12-31 23:46:50 +0200

    more

--- arch_setup.py
+++ arch_setup.py
@@ -8,6 +8,7 @@ import tempfile
 
 HERE = os.path.dirname(__file__) + '/'
 ENVIRONMENT_PATH = '/etc/environment'
+MOUSE_ACCEL_PATH = '/usr/share/X11/xorg.conf.d/90-mouse_accel.conf'
 
 def term(cmds:list):
     cmd = shlex.join(cmds)
@@ -33,7 +34,27 @@ def sudo_backup_file(path):
 
 def main():
 
-    # essential
+    # mouse accel
+    sudo_backup_file(MOUSE_ACCEL_PATH)
+    with tempfile.NamedTemporaryFile('w', delete=False) as f:
+        f.write('''
+Section "InputClass"
+        Identifier "Mouse With No Acceleration"
+        MatchDriver "libinput"
+        MatchIsPointer "yes"
+        Option "AccelProfile" "flat"
+EndSection
+''')
+        name = f.name
+    sudo_move(name, MOUSE_ACCEL_PATH)
+
+    # video drivers
+    pkg_install('lib32-mesa', 'vulkan-radeon', 'lib32-vulkan-radeon', 'vulkan-icd-loader', 'lib32-vulkan-icd-loader')
+
+    # wine deps
+    pkg_install(*'wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader'.split(' '))
+
+    # DE essential
     pkg_install('bspwm', 'sxhkd', 'polybar')
 
     # polybar fonts
