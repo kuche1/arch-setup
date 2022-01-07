babed1ec09a8cf8fe297e9d0b9d10b52d6fbd196
me <you@example.com>
2022-01-07 13:49:36 +0200

    asd

--- arch_setup.py
+++ arch_setup.py
@@ -165,7 +165,9 @@ EndSection
     pkg_install(*'wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader'.split(' '))
 
     # kernel
+    aur_install('update-grub')
     pkg_install('linux-zen', 'linux-zen-headers')
+    term(['sudo', 'update-grub'])
 
     # vmware
     aur_install('vmware-workstation')
@@ -178,6 +180,8 @@ EndSection
     else: mode = 'a'
     with open(VMWARE_PREFERENCES_PATH, mode) as f:
         f.write('\nmks.gl.allowBlacklistedDrivers = "TRUE"\n')
+
+    term(['sync'])
 
 if __name__ == '__main__':
     main()
