b98bfe0ef2c50104ada966c0a8f8702237dff6e6
me <znamsiparolata4esno@abv.bg>
2022-01-21 18:33:03 +0200

    asd

--- minq-arch-setup.py
+++ minq-arch-setup.py
@@ -48,9 +48,14 @@ def term(cmds:list):
     cmd = shlex.join(cmds)
     term_raw(cmd)
 
+def term_yes(cmds:list):
+    assert type(cmds) in (list, tuple)
+    cmd = 'yes | ' + shlex.join(cmds)
+    term_raw(cmds)
+
 def pkg_install(*packages:list[str]):
     assert type(packages) != str
-    term(['yes', '|', 'sudo', 'pacman', '-S', '--needed', '--noconfirm'] + list(packages))
+    term_yes(['sudo', 'pacman', '-S', '--needed', '--noconfirm'] + list(packages))
 
 def aur_install(*packages:list[str]): # TODO check if yay or paru, and if not both install
     assert type(packages) != str
