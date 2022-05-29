8da3e2e005956cf3ccf75ddf8009a08e34bc0ef8
asdfg <znamsiparolata4esno@abv.bg>
2022-05-30 00:35:20 +0300

    asd

--- config/i3/custom-status-command.sh
+++ config/i3/custom-status-command.sh
@@ -1,9 +1,19 @@
 #! /usr/bin/env bash
 
+netspeed_file="$(dirname $0)/net-speed.sh"
+
+updates_regular="$(checkupdates | wc -l)"
+updates_aur="$(paru -Qua | wc -l)"
+updates="$updates_regular&$updates_aur"
+
 i3status | while :
 do
 	read line
+
 	keyboard="$(xkblayout-state print \"%s\")"
 	keyboard="${keyboard:1:-1}"
-	echo "$line | $keyboard"
+
+	netspeed="$($netspeed_file)"
+
+	echo "$updates | ${netspeed}${line} | $keyboard"
 done
