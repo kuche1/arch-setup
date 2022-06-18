8e5e3b46c2b814bab5a589b70b4112b754cb4359
asdfg <znamsiparolata4esno@abv.bg>
2022-06-18 21:50:40 +0300

    asd

---
+++ software/ip-bans/ip-bans.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+#sudo iptables -A INPUT -s 188.165.207.20 -j DROP
+
+import os
+import subprocess
+import shlex
+
+HERE = os.path.dirname(__file__)
+IP_BANS_FILE = os.path.join(HERE, 'ips-to-ban')
+
+with open(IP_BANS_FILE, 'r') as f:
+    cont = f.read()
+
+for ip in cont.splitlines():
+    comment_char = '#'
+    if comment_char in ip:
+        ind = ip.index(comment_char)
+        ip = ip[:ind]
+    ip = ip.strip()
+    if ip == '':
+        continue
+
+    print(f'Blocking IP: {ip}')
+    subprocess.run(shlex.join(['sudo', 'iptables', '-A', 'INPUT', '-s', ip, '-j', 'DROP']), shell=True, check=True)
+
+print('Done')
